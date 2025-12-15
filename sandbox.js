49ac39cfa71865293ad04d20759225e8da1ffa91
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-12-15 14:02:44 -0500

    kl/jkl;k;

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {});
+let Submitbutton = document.getElementById("Submit-button");
+document.addEventListener("DOMContentLoaded", function () {
+  Submitbutton.addEventListener("click", getWords);
+});
+
+let words = document.querySelectorAll('input[type="text"]');
+console.log(words);
+function getWords() {
+  console.log(Submitbutton);
+}
