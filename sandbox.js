96a2856939caccc8aebc9044b19fba1fc20ab214
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-12-16 14:03:42 -0500

    ukk7uyuj

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,16 @@
-let Submitbutton = document.getElementById("Submit-button");
+let submitButton = document.getElementById("Submit-button");
 document.addEventListener("DOMContentLoaded", function () {
-  Submitbutton.addEventListener("click", getWords);
+  submitButton.addEventListener("click", getWords);
 });
 
-let words = document.querySelectorAll('input[type="text"]');
-console.log(words);
 function getWords() {
-  console.log(Submitbutton);
+  let words = document.querySelectorAll('input[type="text"]');
+  
+  console.log(submitButton);
+
+  let placeholders = document.querySelectorAll(".userInput");
+console.log(words[index]0
+  placeholders.forEach((placeholder, index) => {
+    console.log(placeholder, index);
+  });
 }
