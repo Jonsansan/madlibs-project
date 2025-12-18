567af9fea03c5c797ea52c8055d15aaede07e9a8
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-12-18 12:19:38 -0500

    uodate tojpoij

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,22 @@
 let submitButton = document.getElementById("Submit-button");
+
 document.addEventListener("DOMContentLoaded", function () {
   submitButton.addEventListener("click", getWords);
 });
 
 function getWords() {
   let words = document.querySelectorAll('input[type="text"]');
-  
-  console.log(submitButton);
+  // console.log(submitButton);
 
   let placeholders = document.querySelectorAll(".userInput");
-console.log(words[index]0
+
   placeholders.forEach((placeholder, index) => {
-    console.log(placeholder, index);
+    if (index < words.length) {
+      placeholder.innerHTML = words[index].value;
+    }
+  });
+
+  words.forEach((word) => {
+    console.log(word.value);
   });
 }
